6b21bc5aecb23f381cbbf63867cac80998b346fe
Krutonium <PFCKrutonium@gmail.com>
2023-05-12 09:37:52 -0400

    Quotes

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   outputs = {self, nixpkgs}:{
     defaultPackage.x86_64-linux = 
-      with import nixpkgs { system = x86_64-linux; };
+      with import nixpkgs { system = "x86_64-linux"; };
       pkgs.callPackage ./package.nix {};
     };
 }
